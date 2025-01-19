c35c18d54d8803e7f7b968830a6f6b75f025e4e8
venom76568 <hagrawal560@gmail.com>
2025-01-20 02:42:44 +0530

    backend corrected

--- src/Api.js
+++ src/Api.js
@@ -1,13 +1,21 @@
 // Api.js
-import axios from "axios";
-
-const baseURL = "https://expo-backend-55ed.onrender.com";
-
 export const registerUser = async (formData) => {
-  try {
-    const response = await axios.post(`${baseURL}/register`, formData);
-    return response.data;
-  } catch (error) {
-    throw error.response.data;
+  const response = await fetch(
+    "https://expo-backend-public.onrender.com/register",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }
+  );
+
+  // Check if the response is OK, otherwise throw an error
+  if (!response.ok) {
+    throw new Error("Failed to register");
   }
+
+  const responseData = await response.json();
+  return responseData.message; // Expecting 'message' from the backend
 };
